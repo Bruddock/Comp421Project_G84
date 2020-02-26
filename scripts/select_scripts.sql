41685886bedde281eea3bdd00c5f3876cf8abe24
ysuneric <ysuneric@gmail.com>
2020-02-26 15:27:09 -0500

    select scripts

--- scripts/select_scripts.sql
+++ scripts/select_scripts.sql
@@ -1,12 +1,22 @@
 /*select all staff employed on a specific event)*/
-select name, role, employeeid from staff
-where employeeid = (select employeeid from staffby where edate = '2019-02-14' and etime > '08:00:00')
+select staff.employeeid, role, name
+from staff join staffby on staff.employeeid = staffby.employeeid
+where staffby.reservationid = 1093;
 
 /*select unpaid invoices*/
-
+select name, emailaddress
+from customer
+where emailaddress in (select clientemail
+                        from invoice
+                        where descriptionofservices = 'reservation payment' and status = 'pending')
+  and emailaddress IS NOT NULL;
 
 /*select invoices over the year*/
+select invoiceid, date, amount from invoice
+where extract(year from date) = 2019;
 
 /*select all vegetarian menus? or menus with alcoholic*/
+
+
 
 /*select supplier and ingredients involved with name, and invoices with that supplier*/
