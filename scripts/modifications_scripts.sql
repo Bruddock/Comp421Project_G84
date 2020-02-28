600b7290f8c287a9fe5821ac31b9005aa3c07d80
Bruddock <benjamin.ruddock@mail.mcgill.ca>
2020-02-28 12:29:49 -0500

    Update modifications_scripts.sql
    adding last part from q6

--- scripts/modifications_scripts.sql
+++ scripts/modifications_scripts.sql
@@ -1,21 +1,36 @@
 
-/*Complete payment release for pending invoices to Company7 */
+--Complete payment release for pending invoices to Company7 
 UPDATE invoice
 SET status = 'accepted'
 WHERE status = 'pending' AND suppliername = 'Company17';
 
-/*Accept payment from pending customer and update account balances */
+--Accept payment from pending customer and update account balances 
 UPDATE accountreceivable
     SET revenue = revenue + invoice.amount
 FROM invoice
 WHERE accountreceivable.acctid = invoice.acctid AND status = 'pending' AND clientemail is not null ;
-/* Then update all pending invoices to accepted */
+--Then update all pending invoices to accepted 
 UPDATE invoice
 SET status = 'accepted'
 WHERE status = 'pending' AND clientemail is not null ;
 
-/*Change menu 840 to vegan */
+--Change menu 840 to vegan 
 DELETE FROM contains WHERE menuid = 840;
 INSERT INTO contains values (840, 'dish13');
 
+-- Select all duplicates of a name, ingredientname pairing
+SELECT name, ingredientname
+FROM
+    (SELECT name, ingredientname,
+            ROW_NUMBER() OVER( PARTITION BY name, ingredientname
+                ORDER BY  ingredientname ) AS numrows
+     FROM ingredients ) t
+WHERE t.numrows > 1;
+
+--Clone, delete duplicate name, ingredientname pairings and reinstate ingredients.
+CREATE TABLE ingredients_temp (LIKE ingredients);
+
+INSERT INTO ingredients_temp (companyname, name , ordered, received, quantity, ingredientname)
+SELECT DISTINCT ON (name, ingredientname) 
+companyname, name, ordered, received, quantity, ingredientname FROM ingredients;
 
